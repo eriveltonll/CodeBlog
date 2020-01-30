55739c39f0f441e893dcc0fb218c8a1b31c6a3da
Erivelton Laia <erivelton.laia@engdb.com.br>
2020-01-30 14:06:49 -0300

    comitando com alterções

--- src/main/java/com/spring/codeblog/CodeblogApplication.java
+++ src/main/java/com/spring/codeblog/CodeblogApplication.java
@@ -2,12 +2,15 @@ package com.spring.codeblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodeblogApplication {
+public class CodeblogApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeblogApplication.class, args);
+
 	}
 
 }
