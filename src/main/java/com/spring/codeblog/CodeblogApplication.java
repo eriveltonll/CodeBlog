2e0e49a1f6a16a907e2eae3ede7a99d7c78eede5
Erivelton Laia <erivelton.laia@engdb.com.br>
2020-02-18 17:56:39 -0300

    Alteração do arquivo pom e main

--- src/main/java/com/spring/codeblog/CodeblogApplication.java
+++ src/main/java/com/spring/codeblog/CodeblogApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodeblogApplication  {
+public class CodeblogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeblogApplication.class, args);
